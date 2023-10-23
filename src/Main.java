af6c8f852301ebc0149306e0480375ca4a4fbc20
Mjk78 <mira23009@student.hamk.fi>
2023-10-23 22:38:56 +0300

    ensimmäinen kommentti

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,58 @@
 
+import java.util.Scanner;
+
+ 
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        Scanner scanner = new Scanner(System.in);
+
+        String answer = "Emma";
+
+        int guesses = 0;
+
+       
+
+        String guess = "";
+
+       
+
+        do {
+
+            System.out.print("Arvaa kuka olen: ");
+
+            guess = scanner.nextLine();
+
+           
+
+            if (guess.equalsIgnoreCase(answer)) {
+
+                System.out.println("Onneksi olkoon, arvasit oikein!");
+
+                break;
+
+            } else if (guess.equalsIgnoreCase("loppu")) {
+
+                System.out.println("Peli päättyi.");
+
+                break;
+
+            } else {
+
+                System.out.println("Väärin arvattu. Yritä uudestaan.");
+
+                guesses++;
+
+            }
+
+        } while (true);
+
+       
+
+        System.out.println("Hienoa ratkaisit tehtävä, arvasit " + guesses + " kertaa.");
+
+    }
 
 }
